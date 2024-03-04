0930ea431ddd7d99f39b33f951acfd2336899b71
emdadul7171 <emdadul.m4yours@gmail.com>
2024-03-04 22:37:07 +0600

    fix

--- modules/frontend/home/@components/home-hero-two/index.tsx
+++ modules/frontend/home/@components/home-hero-two/index.tsx
@@ -5,7 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 const HomeHero = () => {
   return (
     <section>
-      <div className="min-h-[calc(100vh-64px)] lg:min-h-[calc(100vh-75px)] relative bg-[url('/images/test.jpg')] bg-center bg-no-repeat bg-cover">
+      <div className="min-h-[calc(100vh-68px)] lg:min-h-[calc(100vh-75px)] relative bg-[url('/images/test.jpg')] bg-center bg-no-repeat bg-cover">
         <div
           className="overlay w-full h-full absolute left-0 top-0 z-1"
           // style={{
