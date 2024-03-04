af3dfaa3d79231368857e86968cfb51307c9a0ef
emdadul7171 <emdadul.m4yours@gmail.com>
2024-03-04 22:42:13 +0600

    fix

--- modules/frontend/home/@components/home-hero-two/index.tsx
+++ modules/frontend/home/@components/home-hero-two/index.tsx
@@ -5,7 +5,7 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
 const HomeHero = () => {
   return (
     <section>
-      <div className="min-h-[calc(100vh-68px)] lg:min-h-[calc(100vh-75px)] relative bg-[url('/images/test.jpg')] bg-center bg-no-repeat bg-cover">
+      <div className="min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-75px)] relative bg-[url('/images/test.jpg')] bg-center bg-no-repeat bg-cover">
         <div
           className="overlay w-full h-full absolute left-0 top-0 z-1"
           // style={{
@@ -13,7 +13,7 @@ const HomeHero = () => {
           //     "linear-gradient(180deg, rgba(0, 47, 91, 0) 0%, #002F5B 100%)",
           // }}
         ></div>
-        <div className="max-w-[1020px] px-5 mx-auto min-h-[calc(100vh-64px)] lg:min-h-[calc(100vh-75px)] text-white relative z-10 ">
+        <div className="max-w-[1020px] px-5 mx-auto min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-75px)] text-white relative z-10 ">
           <div className="w-full absolute left-0 bottom-[25vh]">
             <div className="max-w-full lg:max-w-[570px] p-4 lg:p-0 text-center lg:text-left">
               <h1 className="mb-3 text-white">
