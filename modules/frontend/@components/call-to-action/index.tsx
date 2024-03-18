dbc8258ab0292a926ddfb59a89c4d04ba1e8c8a8
emdadul7171 <emdadul.m4yours@gmail.com>
2024-03-18 22:04:09 +0600

    fix

--- modules/frontend/@components/call-to-action/index.tsx
+++ modules/frontend/@components/call-to-action/index.tsx
@@ -1,29 +1,43 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const CallToAction = () => {
   return (
     <section>
       <div className="container">
-        <div className="max-w-[1070px] mx-auto">
-          <div className="grid lg:grid-cols-[554px_auto] items-center justify-between p-5 lg:p-12 bg-secondary lg:mb-[-140px] z-50 relative rounded-md">
+        <div className="grid lg:grid-cols-[496px_1fr] justify-between items-center gap-8 lg:gap-[120px] bg-secondary py-5 lg:py-[45px] px-5 lg:px-[94px] lg:mb-[-140px] z-50 relative rounded-md">
+          <div className="flex flex-col items-center lg:items-start gap-[30px] order-1 lg:order-0">
             <div>
-              <h3 className="text-[28px] text-white mb-[10px]">
-                Contact us now to kickstart your journey!
+              <h3 className="text-white heading-three mb-3 text-center lg:text-start">
+                Lorem Ipsum is simply dummy text of the printing
               </h3>
-              <p className="text-lg text-white">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry
+              <p className="mb-0 text-white text-center lg:text-start">
+                <span className="block">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting
+                </span>
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece
               </p>
             </div>
             <div>
               <Link
-                href=""
-                className="btn bg-primary bg-white text-secondary font-semibold rounded-md"
+                href="/virtual-counselling"
+                className="btn btn-white inline-flex gap-[10px]  rounded-md"
               >
-                Keep Reading
+                <span className="text-lg">Donate</span>
               </Link>
             </div>
+          </div>
+          <div className="order-0 lg:order-1">
+            <Image
+              src="/images/test.jpg"
+              alt="Moves International"
+              width={960}
+              height={540}
+            />
           </div>
         </div>
       </div>
