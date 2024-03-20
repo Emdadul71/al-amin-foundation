cbdfa0eba65d37e948ba35b1f1b068a08b2ee897
emdadul7171 <emdadul.m4yours@gmail.com>
2024-03-20 18:42:43 +0600

    fix

--- modules/frontend/@components/call-to-action/index.tsx
+++ modules/frontend/@components/call-to-action/index.tsx
@@ -7,31 +7,23 @@ const CallToAction = () => {
   return (
     <section>
       <div className="container">
-        <div className="max-w-[1070px] mx-auto grid lg:grid-cols-[1fr_1fr] justify-between items-center gap-8 lg:gap-[40px] bg-secondary py-5 lg:py-[45px] px-5 lg:px-[50px] lg:mb-[-150px] z-10 relative rounded-md">
-          <div className="order-0 lg:order-1">
-            <Image
-              src="/images/test.jpg"
-              alt="Moves International"
-              width={960}
-              height={540}
-            />
-          </div>
-          <div className="flex flex-col items-center lg:items-start gap-[30px] order-1 lg:order-0">
+        <div className="max-w-[1070px] mx-auto lg:mb-[-150px] z-10 relative">
+          <div className="grid lg:grid-cols-[554px_auto] items-center justify-between p-5 lg:p-12 bg-secondary rounded-md ">
             <div>
-              <h3 className="text-white heading-three mb-3 text-center lg:text-start">
-                Lorem Ipsum is simply dummy text of the printing
+              <h3 className="text-[28px] text-white mb-[10px]">
+                Contact us now to kickstart your journey!
               </h3>
-              <p className="mb-0 text-white text-center lg:text-start">
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece
+              <p className="text-lg text-white">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has
               </p>
             </div>
             <div>
               <Link
-                href="/virtual-counselling"
-                className="btn btn-white inline-flex gap-[10px]  rounded-md"
+                href=""
+                className="btn bg-primary bg-white text-secondary font-semibold rounded-md"
               >
-                <span className="text-lg">Donate</span>
+                Keep Reading
               </Link>
             </div>
           </div>
